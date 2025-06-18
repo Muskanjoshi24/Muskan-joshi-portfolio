5440c30ca46bb065b19678b7fcdb2860221e2d6b
Muskan Joshi <m8sk2nj@gmail.com>
2025-06-18 13:34:31 +0530

    Responsive nav-bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,11 @@
-import { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  const [showMenu, setShowMenu] = useState(false);
+  
 
   return (
     <nav className="navbar">
-      <h1 className="logo">Muskan</h1>
-      <div className="menu-icon" onClick={() => setShowMenu(!showMenu)}>
-        ☰
-      </div>
-      <ul className={showMenu ? "nav-links show" : "nav-links"}>
+      <ul className="nav-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
